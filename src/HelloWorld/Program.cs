6292ce7ed3c0941f7128182f7efdee5094ad1b4c
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-11-27 16:09:12 +0800

    feature：杀入

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,3 +1,51 @@
-﻿using System;
+﻿using AlarmCenterGrpcServiceLibrary;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HelloWorld.DI.FrameworkDI;
 
-Console.WriteLine("Hello World!");
+namespace HelloWorld
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await LoginAsync();
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        static async Task LoginAsync()
+        {
+            using var channel = new GrpcChannel("127.0.0.1:4000", ChannelCredentials.Insecure);
+
+            var helloRequest = new HelloRequest
+            {
+                Name = "Hello World!",
+            };
+
+            var helloReply = await GreeterClient.SayHelloAsync(helloRequest);
+            Console.WriteLine(helloReply.Message);
+        }
+
+        /// <summary>
+        /// 设备
+        /// </summary>
+        static void Equip()
+        {
+
+        }
+
+        /// <summary>
+        /// 表
+        /// </summary>
+        static void Table()
+        {
+
+        }
+    }
+}
